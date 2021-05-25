686d9c9dfbfda66e3e332564f34c0bee56751a5f
axiomattik <axiomattik@pocketgecko.co.uk>
2021-05-25 10:58:27 +0100

    fix tab resize bug

--- src/js/tab-height.js
+++ src/js/tab-height.js
@@ -1,17 +1,31 @@
 (function() {
 	/* ensure a uniform height for woocommerce tab panels */
 
-	let tabpanels = document.querySelectorAll("div.woocommerce-Tabs-panel");
+	function adjustTabPanelHeights() {
+		let tabpanels = document.querySelectorAll("div.woocommerce-Tabs-panel");
+		let baseFontSize = window.getComputedStyle(
+			document.getRootNode().children[0]
+		).fontSize.replace("px", "");
 
-	// find the maximum height
-	let maxHeight = 0;
-	for (let tb of tabpanels) {
-		if ( tb.offsetHeight > maxHeight ) maxHeight = tb.offsetHeight;
+		// recalculate automatic heights of tab panels 
+		// and find the maximum height
+		let maxHeight = 0;
+		for (let tb of tabpanels) {
+			let tmpdisplay = tb.style.display;
+			tb.style.display = "block";
+			tb.style.height = "auto";
+			if ( tb.offsetHeight > maxHeight ) maxHeight = tb.offsetHeight;
+			tb.style.display = tmpdisplay;
+		}
+
+		// set all tab panels to have that height
+		let height = (maxHeight / baseFontSize).toString() + "rem";
+		for (let tb of tabpanels) {
+			tb.style.height = height;
+		}
 	}
 
-	// set all tab panels to have that height
-	for (let tb of tabpanels) {
-		tb.style.height = maxHeight.toString() + "px";
-	}
+	window.addEventListener("load", adjustTabPanelHeights);
+	window.addEventListener("resize", adjustTabPanelHeights);
 
 })();
