1cbfe2c050b94d661f185dc4ba0fd9fe408ecf4a
axiomattik <axiomattik@pocketgecko.co.uk>
2021-07-08 09:48:06 +0100

    replace search form template and styling

---
+++ src/searchform.php
@@ -0,0 +1,12 @@
+<form role="search" ' . $aria_label . 'method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ); ?></span>
+	</label>
+
+	<input 
+		type="search" 
+		class="search-field" 
+		value="<?php echo get_search_query(); ?>"  name="s" />
+
+	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+</form>
