6ec8536e11acfe6b906f881b726d814389c19881
axiomattik <axiomattik@protonmail.com>
2024-03-15 10:17:13 +0000

    put the header image in the right place

--- src/front-page.php
+++ src/front-page.php
@@ -4,15 +4,13 @@
  */
 get_header();
 ?>
-<main id="primary" class="site-main">
 	<?php if ( has_header_image() ): ?>
 	<div id="hero">
 		<?php the_header_image_tag(); ?>
 	</div>
 	<?php endif; ?>
-	<div id="featured-products">
 
-	</div>
+<main id="primary" class="site-main">
 
 	<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 		<?php the_content(); ?>
